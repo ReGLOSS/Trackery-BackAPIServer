5e95bd6ccc81527ba1155989da19cdcb429b933a
한제우 <hanjaewoo98@gmail.com>
2025-01-20 21:52:56 +0900

    initial commit

---
+++ src/main/java/com/trackery/trackerybackapiserver/TrackeryBackApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.trackery.trackerybackapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackeryBackApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackeryBackApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackery/trackerybackapiserver/TrackeryBackApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackery.trackerybackapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackeryBackApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
