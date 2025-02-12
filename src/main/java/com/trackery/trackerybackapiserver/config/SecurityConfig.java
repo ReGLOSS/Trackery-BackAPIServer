358eb507b1e7da9bfbc59456981ee418efe353bb
durururuk <147094944+durururuk@users.noreply.github.com>
2025-02-12 18:12:50 +0900

    feature/회원가입/ 임시 시큐리티 전체 허용 해제, dto 검증을 위한 spring-boot-starter-validation 종속성 추가

--- src/main/java/com/trackery/trackerybackapiserver/config/SecurityConfig.java
+++ src/main/java/com/trackery/trackerybackapiserver/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +14,12 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/api/**").permitAll()
+				.requestMatchers("/api/users/register").permitAll()
 				.requestMatchers("/").permitAll()
-				.anyRequest().permitAll()
+				.anyRequest().authenticated()
 			)
-			.csrf(csrf -> csrf.disable())
-			.formLogin(form -> form.disable()
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable
 			).build();
 	}
 }
