8f2d1227852e4e91206890ad20336e47eaff2843
durururuk <147094944+durururuk@users.noreply.github.com>
2025-02-13 15:22:24 +0900

    feature/공통-응답/ 기본 응답 포맷 작성

---
+++ src/main/java/com/trackery/trackerybackapiserver/domain/common/response/ApiResponse.java
@@ -0,0 +1,36 @@
+package com.trackery.trackerybackapiserver.domain.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.trackery.trackerybackapiserver.domain.common.response.enums.ErrorCode;
+import com.trackery.trackerybackapiserver.domain.common.response.enums.SuccessCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"code", "message", "data"})
+public class ApiResponse<T> {
+
+	private final int code;
+	private final String message;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private T data;
+
+	public static <T> ApiResponse<T> success(SuccessCode successCode, T data) {
+		return new ApiResponse<>(successCode.getCode(), successCode.getMessage(), data);
+	}
+
+	public static <T> ApiResponse<T> success(SuccessCode successCode) {
+		return success(successCode, null);
+	}
+
+	public static <T> ApiResponse<T> error(ErrorCode errorCode, T data) {
+		return new ApiResponse<>(errorCode.getCode(), errorCode.getMessage(), data);
+	}
+
+	public static <T> ApiResponse<T> error(ErrorCode errorCode) {
+		return error(errorCode, null);
+	}
+}
---
+++ src/main/java/com/trackery/trackerybackapiserver/domain/common/response/exception/ApiException.java
@@ -0,0 +1,5 @@
+package com.trackery.trackerybackapiserver.domain.common.response.exception;
+
+public class ApiException extends RuntimeException {
+
+}
