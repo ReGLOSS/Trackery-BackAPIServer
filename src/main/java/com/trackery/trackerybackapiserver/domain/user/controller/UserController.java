bcd8fb5520bc0d036ad01f32959a01a23e122dca
durururuk <147094944+durururuk@users.noreply.github.com>
2025-02-14 14:55:01 +0900

    feature/회원-가입/ 닉네임 중복 체크하는 api 구현

--- src/main/java/com/trackery/trackerybackapiserver/domain/user/controller/UserController.java
+++ src/main/java/com/trackery/trackerybackapiserver/domain/user/controller/UserController.java
@@ -2,14 +2,16 @@ package com.trackery.trackerybackapiserver.domain.user.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trackery.trackerybackapiserver.domain.common.response.ApiResponse;
 import com.trackery.trackerybackapiserver.domain.common.response.enums.SuccessCode;
-import com.trackery.trackerybackapiserver.domain.user.dto.UserDto;
+import com.trackery.trackerybackapiserver.domain.user.dto.UserRegisterDto;
 import com.trackery.trackerybackapiserver.domain.user.service.UserService;
 
 import jakarta.validation.Valid;
@@ -24,10 +26,15 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public ResponseEntity<ApiResponse<String>> registerUser(@Valid @RequestBody UserDto userDto) {
-		userService.registerUser(userDto);
+	public ResponseEntity<ApiResponse<String>> registerUser(@Valid @RequestBody UserRegisterDto userRegisterDto) {
+		userService.registerUser(userRegisterDto);
 		return ResponseEntity
 			.status(HttpStatus.CREATED)
 			.body(ApiResponse.success(SuccessCode.CREATED));
+	}
+
+	@GetMapping("/username-availability")
+	public ResponseEntity<ApiResponse<Boolean>> checkUsernameAvailability(@RequestParam String username) {
+		return ResponseEntity.ok(ApiResponse.success(SuccessCode.OK, userService.checkUsernameAvailability(username)));
 	}
 }
--- src/main/java/com/trackery/trackerybackapiserver/domain/user/mapper/UserMapper.java
+++ src/main/java/com/trackery/trackerybackapiserver/domain/user/mapper/UserMapper.java
@@ -7,4 +7,6 @@ import com.trackery.trackerybackapiserver.domain.user.entity.User;
 @Mapper
 public interface UserMapper {
 	void insertUser(User user);
+
+	int checkUsernameAvailability(String username);
 }
