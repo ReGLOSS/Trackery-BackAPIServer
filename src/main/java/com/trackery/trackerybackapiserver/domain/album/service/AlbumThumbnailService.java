0a864ab7a329d9168c269eec743f21acda4a3ee6
durururuk <147094944+durururuk@users.noreply.github.com>
2025-06-20 15:46:42 +0900

    feature/album-thumbnail/ 앨범 썸네일 자동 생성 로직 작성

--- src/main/java/com/trackery/trackerybackapiserver/domain/album/service/AlbumThumbnailService.java
+++ src/main/java/com/trackery/trackerybackapiserver/domain/album/service/AlbumThumbnailService.java
@@ -1,8 +1,16 @@
 package com.trackery.trackerybackapiserver.domain.album.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.trackery.trackerybackapiserver.domain.album.dto.response.AlbumImageEditResponseDto;
+import com.trackery.trackerybackapiserver.domain.album.entity.Album;
+import com.trackery.trackerybackapiserver.domain.album.entity.AlbumImage;
+import com.trackery.trackerybackapiserver.domain.album.enums.AlbumImageEditOperation;
 import com.trackery.trackerybackapiserver.domain.album.mapper.AlbumMapper;
+import com.trackery.trackerybackapiserver.domain.common.response.enums.ErrorCode;
+import com.trackery.trackerybackapiserver.domain.common.response.exception.ApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,4 +29,49 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AlbumThumbnailService {
 	private final AlbumMapper albumMapper;
+
+	public void handleAlbumThumbnailChange(AlbumImageEditResponseDto dto, AlbumImageEditOperation operation) {
+		switch (operation) {
+			case ADD:
+				setAlbumThumbnailForAdd(dto);
+				break;
+			case DELETE:
+				changeAlbumThumbnailForDeleteImageFromAlbum(dto);
+				break;
+			default:
+				throw new ApiException(ErrorCode.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	public void setAlbumThumbnailForAdd(AlbumImageEditResponseDto dto) {
+		Album album = albumMapper.findByAlbumId(dto.getAlbumId()).orElseThrow(
+			() -> new ApiException(ErrorCode.NOT_FOUND_ALBUM)
+		);
+
+		if (album.getThumbnailImageId() != null) {
+			return;
+		}
+
+		Long firstRegisteredImageId = dto.getSucceededImageIds().stream().findFirst().orElseThrow(
+			() -> new ApiException(ErrorCode.INTERNAL_SERVER_ERROR)
+		);
+
+		albumMapper.setThumbnail(album.getAlbumId(), firstRegisteredImageId);
+	}
+
+	public void changeAlbumThumbnailForDeleteImageFromAlbum(AlbumImageEditResponseDto dto) {
+		Album album = albumMapper.findByAlbumId(dto.getAlbumId()).orElseThrow(
+			() -> new ApiException(ErrorCode.NOT_FOUND_ALBUM)
+		);
+
+		if (!dto.getSucceededImageIds().contains(album.getThumbnailImageId())) {
+			return;
+		}
+
+		List<AlbumImage> albumImageList = albumMapper.findAlbumImagesByAlbumId(album.getAlbumId());
+
+		Long newThumbnailImageId = albumImageList.stream().findFirst().map(AlbumImage::getImageId).orElse(null);
+
+		albumMapper.setThumbnail(album.getAlbumId(), newThumbnailImageId);
+	}
 }
