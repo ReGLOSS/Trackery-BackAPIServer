8545c84d016266b677c001e276f315f98b815d40
Nari-Lee <serika9009@gmail.com>
2025-07-02 17:01:18 +0900

    feature/tag-구현 sql 기본 태그 추가

--- src/main/resources/data/0.2 insert_default/insert_default_data_2025_07_01.sql
+++ src/main/resources/data/0.2 insert_default/insert_default_data_2025_07_01.sql
@@ -11,6 +11,8 @@ INSERT INTO role (role_id, role_name) VALUES
                                           (1, 'USER'),
                                           (2, 'MANAGER');
 
+-- tag_type: 1 (위치)
+
 -- 시도 기본 태그 인서트 (juso_sido 테이블에서 자동 생성)
 INSERT INTO tag (tag_name, tag_type, tag_use_count, created_at)
 SELECT sd_name, 1, 0, NOW()
@@ -22,3 +24,24 @@ INSERT INTO tag (tag_name, tag_type, tag_use_count, created_at)
 SELECT sgg_name, 1, 0, NOW()
 FROM juso_sigungu
 ORDER BY sgg_id;
+
+-- tag_type: 2 (계절)
+INSERT INTO tag (tag_name, tag_type, tag_use_count, created_at) VALUES
+                                                                    ('봄', 2, 0, NOW()),
+                                                                    ('여름', 2, 0, NOW()),
+                                                                    ('가을', 2, 0, NOW()),
+                                                                    ('겨울', 2, 0, NOW());
+
+-- tag_type: 3 (시간)
+INSERT INTO tag (tag_name, tag_type, tag_use_count, created_at) VALUES
+                                                                    ('아침', 3, 0, NOW()),
+                                                                    ('점심', 3, 0, NOW()),
+                                                                    ('저녁', 3, 0, NOW()),
+                                                                    ('밤', 3, 0, NOW());
+
+-- tag_type: 4 (날씨)
+INSERT INTO tag (tag_name, tag_type, tag_use_count, created_at) VALUES
+                                                                    ('맑음', 4, 0, NOW()),
+                                                                    ('흐림', 4, 0, NOW()),
+                                                                    ('비', 4, 0, NOW()),
+                                                                    ('눈', 4, 0, NOW());
