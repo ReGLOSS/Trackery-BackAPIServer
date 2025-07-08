3d4c9daf0a7d059274917c9779e597df7b1d9a84
Nari-Lee <serika9009@gmail.com>
2025-07-08 23:02:06 +0900

    feature/tag-구현 테스트코드 추가 및 문서화

--- src/test/java/com/trackery/trackerybackapiserver/domain/image/controller/ImageUploadControllerTest.java
+++ src/test/java/com/trackery/trackerybackapiserver/domain/image/controller/ImageUploadControllerTest.java
@@ -1,12 +1,14 @@
 package com.trackery.trackerybackapiserver.domain.image.controller;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -76,7 +78,7 @@ class ImageUploadControllerTest extends CommonMockMvcControllerTestSetUp {
 		org.springframework.test.util.ReflectionTestUtils.setField(imageUploadDto, "imageName", "test-image.jpg");
 		org.springframework.test.util.ReflectionTestUtils.setField(imageUploadDto, "imageType", "JPEG");
 		org.springframework.test.util.ReflectionTestUtils.setField(imageUploadDto, "description", "테스트 이미지 설명");
-		org.springframework.test.util.ReflectionTestUtils.setField(imageUploadDto, "tags", "여행,서울");
+		org.springframework.test.util.ReflectionTestUtils.setField(imageUploadDto, "regionalTags", List.of("여행", "서울"));
 		org.springframework.test.util.ReflectionTestUtils.setField(imageUploadDto, "longitude", 127.0276);
 		org.springframework.test.util.ReflectionTestUtils.setField(imageUploadDto, "latitude", 37.4979);
 		org.springframework.test.util.ReflectionTestUtils.setField(imageUploadDto, "dateString", "2025-01-01T10:30:00");
@@ -97,7 +99,7 @@ class ImageUploadControllerTest extends CommonMockMvcControllerTestSetUp {
 					fieldWithPath("imageName").description("이미지 파일명"),
 					fieldWithPath("imageType").description("이미지 타입 (JPEG, PNG 등)"),
 					fieldWithPath("description").description("이미지 설명"),
-					fieldWithPath("tags").description("이미지 태그 (쉼표로 구분)"),
+					fieldWithPath("regionalTags").description("이미지 지역 태그 목록"),
 					fieldWithPath("longitude").description("경도"),
 					fieldWithPath("latitude").description("위도"),
 					fieldWithPath("dateString").description("이미지 촬영 일시 (ISO 형식)"),
