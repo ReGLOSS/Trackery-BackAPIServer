2be689b781e55a2c17634ef2b54c3f978e5229a7
Nari-Lee <serika9009@gmail.com>
2025-07-10 18:15:16 +0900

    feature/tag-구현 태그 생성시 시간 사용안하게 제거

--- src/main/java/com/trackery/trackerybackapiserver/domain/tag/dto/TagDateRequestDto.java
+++ src/main/java/com/trackery/trackerybackapiserver/domain/tag/dto/TagDateRequestDto.java
@@ -1,27 +1,27 @@
 package com.trackery.trackerybackapiserver.domain.tag.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
  * packageName    : com.trackery.trackerybackapiserver.domain.tag.dto
- * fileName       : TagTimeRequestDto
+ * fileName       : TagDateRequestDto
  * author         : inari
- * date           : 25. 7. 7.
- * description    : 날짜/시간 기반 기본 태그 생성 요청 DTO 클래스입니다.
+ * date           : 25. 7. 10.
+ * description    : 날짜 기반 기본 태그 생성 요청 DTO 클래스입니다.
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
- * 25. 7. 7.        inari       최초 생성
- * 25. 7. 8.        inari       AllArgsConstructor 추가
+ * 25. 7. 10.        inari       최초 생성
  */
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class TagTimeRequestDto {
-	/**
-	 * 날짜/시간 문자열 (기본 태그 생성용).
-	 */
-	private String dateTime;
+public class TagDateRequestDto {
+
+	@NotBlank(message = "날짜는 필수입니다.")
+	private String date;
 }
