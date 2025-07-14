fa79a4b10997cd914a264fbe743d80eff124977e
Nari-Lee <serika9009@gmail.com>
2025-07-14 17:22:08 +0900

    feature/태그-수정 locationName을 sdName + sggName으로 수정

--- src/main/java/com/trackery/trackerybackapiserver/domain/location/dto/LocationNameResponseDto.java
+++ src/main/java/com/trackery/trackerybackapiserver/domain/location/dto/LocationNameResponseDto.java
@@ -14,12 +14,18 @@ import lombok.Getter;
  * -----------------------------------------------------------
  * 25. 7. 7.        inari       최초 생성
  * 25. 7. 11.       inari       태그 리스트 삭제
+ * 25. 7. 14.       inari       locationName을 sdName과 sggName으로 분리
  */
 @Getter
 @Builder
 public class LocationNameResponseDto {
 	/**
-	 * 위치명.
+	 * 시도명.
 	 */
-	private String locationName;
+	private String sdName;
+
+	/**
+	 * 시군구명.
+	 */
+	private String sggName;
 }
