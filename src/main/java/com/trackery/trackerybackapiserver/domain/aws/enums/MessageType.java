b67a9a80252c59069378a13bd43ff033b056f84d
durururuk <durururuk@gmail.com>
2025-08-07 17:06:06 +0900

    feature/feature/user-profile-pic/sqs 메시지 직렬화용 DTO 생성

---
+++ src/main/java/com/trackery/trackerybackapiserver/domain/aws/enums/MessageType.java
@@ -0,0 +1,17 @@
+package com.trackery.trackerybackapiserver.domain.aws.enums;
+
+/**
+ * packageName    : com.bokkurin.trackery.enums
+ * fileName       : MessageType
+ * author         : durururuk
+ * date           : 25. 8. 6.
+ * description    : 메시지 타입
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 25. 8. 6.      durururuk       최초 생성
+ */
+public enum MessageType {
+	SINGLE_IMAGE,
+	PROCESSING_FAILED
+}
