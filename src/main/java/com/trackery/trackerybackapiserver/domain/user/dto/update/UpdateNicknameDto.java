e6dac4cfdf5b920bb73b0adc82327c2deb454787
Nari-Lee <serika9009@gmail.com>
2025-08-07 14:14:50 +0900

    feature/유저-중복-유효성 유저 아이디, 닉네임 유효성 추가

--- src/main/java/com/trackery/trackerybackapiserver/domain/user/dto/update/UpdateNicknameDto.java
+++ src/main/java/com/trackery/trackerybackapiserver/domain/user/dto/update/UpdateNicknameDto.java
@@ -1,6 +1,7 @@
 package com.trackery.trackerybackapiserver.domain.user.dto.update;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,11 +17,13 @@ import lombok.NoArgsConstructor;
  * 25. 4. 11.		durururuk		최초 생성
  * 25. 4. 12.		durururuk		닉네임 변경 기능 구현
  * 25. 4. 12.		durururuk		유저명 변경 기능 구현
- * 25. 4. 14.		durururuk		UpdateUserInfo Controller, Serivce 단위테스트 작성
+ * 25. 4. 14.		durururuk		UpdateUserInfo Controller, Service 단위테스트 작성
+ * 25. 8. 7.		inari			닉네임 패턴 추가
  */
 @Getter
 @NoArgsConstructor
 public class UpdateNicknameDto {
 	@NotBlank
+	@Pattern(regexp = "^.{1,20}$", message = "닉네임은 최대 20자까지 입력 가능합니다.")
 	private String nickname;
 }
--- src/main/java/com/trackery/trackerybackapiserver/domain/user/dto/update/UpdateUserNameDto.java
+++ src/main/java/com/trackery/trackerybackapiserver/domain/user/dto/update/UpdateUserNameDto.java
@@ -1,5 +1,7 @@
 package com.trackery.trackerybackapiserver.domain.user.dto.update;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,9 +15,12 @@ import lombok.NoArgsConstructor;
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 25. 4. 12.		durururuk		최초 생성
+ * 25. 8. 7.		inari			아이디 패턴 추가
  */
 @Getter
 @NoArgsConstructor
 public class UpdateUserNameDto {
+	@NotBlank
+	@Pattern(regexp = "^[a-zA-Z0-9_]{4,15}$", message = "아이디는 4~15자 길이에 영문 대소문자, 숫자, 밑줄(_)로 작성해주세요.")
 	private String userName;
 }
